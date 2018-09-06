b66f6154735447c2c22dff309ec79bea4fc39034
andrefleifel <19fleifela63@stu.smuhsd.org>
2018-09-06 09:51:40 -0700

    class

--- Access Calculate/src/DoMath.java
+++ Access Calculate/src/DoMath.java
@@ -1,8 +1,12 @@
-
+/* This is a client code for the Calculate Library
+ * @Author Andre Fleifel
+ * @Version 1.0
+ * @Date 8-6-18
+ */
 public class DoMath {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
 
 	}
 
