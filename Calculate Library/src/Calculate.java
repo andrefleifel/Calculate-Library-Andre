f761788ddc31785d6614ab0a3bed727c2a4f623a
andrefleifel <19fleifela63@stu.smuhsd.org>
2018-09-06 09:50:49 -0700

    class

---
+++ Calculate Library/src/Calculate.java
@@ -0,0 +1,12 @@
+/*
+ * This class contains the methods to do various math related task
+ * @author Andre Fleifel
+ * @version 1.0
+ * @date 8-6-18
+ */
+public class Calculate {
+
+	public static int square(int number) {
+		return number*number;
+	}
+}	
